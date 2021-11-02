bea7d89e373d43058a222db23ededa69129cacc9
susu <cxd10285052@163.com>
2021-11-02 21:39:06 +0800

    nothing

--- condition_test.py
+++ condition_test.py
@@ -1,7 +1,25 @@
 from threading import Thread
 
+
 class TianMao(Thread):
-    pass
+    def __init__(self):
+        super().__init__(name='天猫精灵')
+
+    def run(self):
+        print('{}:小爱同学'.format(self.name))
+
 
 class XiaoAi(Thread):
-    pass
+    def __init__(self):
+        super().__init__(name='小爱')
+
+    def run(self):
+        print('{}:在'.format(self.name))
+
+
+if __name__ == '__main__':
+    xiao_ai = XiaoAi()
+    tian_mao = TianMao()
+
+    tian_mao.start()
+    xiao_ai.start()
