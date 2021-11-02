5f05742f31a252c49ef2d5ee4abeed0eccb4228f
susu <cxd10285052@163.com>
2021-11-02 22:42:55 +0800

    nothing

--- condition_test.py
+++ condition_test.py
@@ -1,25 +1,43 @@
-from threading import Thread
+import threading
+from threading import Thread, Condition
 
 
 class TianMao(Thread):
-    def __init__(self):
+    def __init__(self, cond):
         super().__init__(name='天猫精灵')
+        self.cond = cond
 
     def run(self):
-        print('{}:小爱同学'.format(self.name))
+        with self.cond:
+            print('{}:小爱同学'.format(self.name))
+            self.cond.notify()
+            self.cond.wait()
+
+            print('{}:我们来对古诗吧'.format(self.name))
+            self.cond.notify()
+            self.cond.wait()
 
 
 class XiaoAi(Thread):
-    def __init__(self):
+    def __init__(self, cond):
         super().__init__(name='小爱')
+        self.cond = cond
 
     def run(self):
-        print('{}:在'.format(self.name))
+        with self.cond:
+            self.cond.wait()
+            print('{}:在'.format(self.name))
+            self.cond.notify()
+
+            self.cond.wait()
+            print('{}:好啊'.format(self.name))
+            self.cond.notify()
 
 
 if __name__ == '__main__':
-    xiao_ai = XiaoAi()
-    tian_mao = TianMao()
+    cond = threading.Condition()
+    xiao_ai = XiaoAi(cond)
+    tian_mao = TianMao(cond)
 
-    tian_mao.start()
     xiao_ai.start()
+    tian_mao.start()
