f98affaf8f235862176b0bf2be0d245908842698
susu <cxd10285052@163.com>
2021-11-03 08:34:43 +0800

    socket客户端案例

--- socket_client.py
+++ socket_client.py
@@ -3,6 +3,7 @@ import socket
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(('127.0.0.1', 8000))
 
+
 while True:
     re_data = input()
     client.send(re_data.encode('utf8'))
